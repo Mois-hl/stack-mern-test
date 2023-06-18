3ef16f502b91140822d6c406a3b082ceac75a895
Mois-hl <moisesae182@gmail.com>
2023-06-17 18:07:14 -0600

    variables

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 import { createPool } from 'mysql2/promise';
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER
+} from './config.js'
 
 export const pool = new createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'testCourseMern'
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
 })
